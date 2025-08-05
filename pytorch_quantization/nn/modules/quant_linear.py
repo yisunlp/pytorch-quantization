57136a50fda10f9725519ebebb5c005e0043f5b8
苏仪 <suyi@suyideMacBook-Air.local>
2025-08-05 15:55:17 +0800

    update

--- pytorch_quantization/nn/modules/quant_linear.py
+++ pytorch_quantization/nn/modules/quant_linear.py
@@ -72,22 +72,23 @@ class QuantLinear(nn.Linear, _utils.QuantMixin):
 
     def forward(self, input):
         if not self.training and self.dynamic_input:
-            input_abs_max = torch.max(torch.abs(input), dim=-1, keepdim=True)[0]
-            input_scale = (input_abs_max / 127.0).clamp(min=1e-8)
-            input = input / input_scale
-            # Scale the weight to match the input scale, now default True, need to set dynamic_input=True for Linear
-            # if True: # Now weight need to scale dynamically because the weight btq and TRT refit operation
-            #     weight_abs_max = torch.max(torch.abs(self.weight), dim=-1, keepdim=True)[0]
-            #     weight_scale = (weight_abs_max / 127.0).clamp(min=1e-8)
-            #     weight = self.weight / weight_scale
+            output = F.linear(input, self.weight, bias=self.bias)
+            # input_abs_max = torch.max(torch.abs(input), dim=-1, keepdim=True)[0]
+            # input_scale = (input_abs_max / 127.0).clamp(min=1e-8)
+            # input = input / input_scale
+            # # Scale the weight to match the input scale, now default True, need to set dynamic_input=True for Linear
+            # # if True: # Now weight need to scale dynamically because the weight btq and TRT refit operation
+            # #     weight_abs_max = torch.max(torch.abs(self.weight), dim=-1, keepdim=True)[0]
+            # #     weight_scale = (weight_abs_max / 127.0).clamp(min=1e-8)
+            # #     weight = self.weight / weight_scale
 
-            quant_input = self._input_quantizer(input)
-            quant_weight = self._weight_quantizer(self.weight)
+            # quant_input = self._input_quantizer(input)
+            # quant_weight = self._weight_quantizer(self.weight)
 
-            output = F.linear(quant_input, quant_weight)
-            output = output * input_scale
-            if self.bias is not None:
-                output += self.bias.unsqueeze(0)
+            # output = F.linear(quant_input, quant_weight)
+            # output = output * input_scale
+            # if self.bias is not None:
+            #     output += self.bias.unsqueeze(0)
         else:
             # Defultly, use our true quantization and Linear function to accelerate the training
             # Instead of using the torch.nn.functional.linear with bf16/fp16, we use FP8/INT8 kernel for GEMM
