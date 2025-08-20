705aca36baedff075099aa1c0937b12dde3d3ce0
苏仪 <suyi@suyideMacBook-Air.local>
2025-08-20 17:07:27 +0800

    update

--- pytorch_quantization/nn/modules/quant_linear.py
+++ pytorch_quantization/nn/modules/quant_linear.py
@@ -75,7 +75,7 @@ class QuantLinear(nn.Linear, _utils.QuantMixin):
             input_abs_max = torch.max(torch.abs(input), dim=-1, keepdim=True)[0]
             input_scale = (input_abs_max.clamp(min=1e-5) / 127.0)
             input = input / input_scale
-            print(input.abs().max().values)
+            print(input.abs().max())
             # Scale the weight to match the input scale, now default True, need to set dynamic_input=True for Linear
             # if True: # Now weight need to scale dynamically because the weight btq and TRT refit operation
             #     weight_abs_max = torch.max(torch.abs(self.weight), dim=-1, keepdim=True)[0]
