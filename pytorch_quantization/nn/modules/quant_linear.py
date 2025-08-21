7f27d13878235a5bcba17d008dfc08ca98156cc5
苏仪 <suyi@suyideMacBook-Air.local>
2025-08-21 17:33:28 +0800

    update

--- pytorch_quantization/nn/modules/quant_linear.py
+++ pytorch_quantization/nn/modules/quant_linear.py
@@ -72,10 +72,10 @@ class QuantLinear(nn.Linear, _utils.QuantMixin):
 
     def forward(self, input):
         if not self.training and self.dynamic_input:
-            input_abs_max = torch.max(torch.abs(input), dim=-1, keepdim=True)[0]
-            input_scale = (input_abs_max.clamp(min=1e-5) / 127.0)
-            input = (input / input_scale).clamp(-127.0, 127.0)
-            print(input.abs().max())
+            # input_abs_max = torch.max(torch.abs(input), dim=-1, keepdim=True)[0]
+            # input_scale = (input_abs_max.clamp(min=1e-5) / 127.0)
+            # input = (input / input_scale).clamp(-127.0, 127.0)
+            # print(input.abs().max())
             # Scale the weight to match the input scale, now default True, need to set dynamic_input=True for Linear
             # if True: # Now weight need to scale dynamically because the weight btq and TRT refit operation
             #     weight_abs_max = torch.max(torch.abs(self.weight), dim=-1, keepdim=True)[0]
@@ -86,7 +86,7 @@ class QuantLinear(nn.Linear, _utils.QuantMixin):
             quant_weight = self._weight_quantizer(self.weight)
 
             output = F.linear(quant_input, quant_weight)
-            output = output * input_scale
+            #output = output * input_scale
             if self.bias is not None:
                 output += self.bias.unsqueeze(0)
         else:
