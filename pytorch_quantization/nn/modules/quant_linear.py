7c63b92884764346c3190f8ff1861043417d6a7a
苏仪 <suyi@suyideMacBook-Air.local>
2025-09-15 15:52:31 +0800

    update

--- pytorch_quantization/nn/modules/quant_linear.py
+++ pytorch_quantization/nn/modules/quant_linear.py
@@ -73,7 +73,7 @@ class QuantLinear(nn.Linear, _utils.QuantMixin):
     def forward(self, input):
         if not self.training and self.dynamic_input:
             dtype=input.dtype
-            input_abs_max = torch.max(torch.abs(input), dim=-1, keepdim=True)[0]
+            input_abs_max = torch.max(torch.abs(input[:,:input.shape[1]//4]), dim=-1, keepdim=True)[0]
             input_scale = (input_abs_max.clamp(min=1e-6) / 127.0)
             input = (input.float() / input_scale.float()).clamp(-127.0, 127.0).to(dtype)
             #Scale the weight to match the input scale, now default True, need to set dynamic_input=True for Linear
