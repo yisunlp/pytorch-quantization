a65af77e9ecfaf2f193f234aba66c557f816e119
苏仪 <suyi@suyideMacBook-Air.local>
2025-09-18 14:06:14 +0800

    update

--- pytorch_quantization/nn/modules/quant_linear.py
+++ pytorch_quantization/nn/modules/quant_linear.py
@@ -74,13 +74,13 @@ class QuantLinear(nn.Linear, _utils.QuantMixin):
         if not self.training and self.dynamic_input:
             dtype=input.dtype
             dim = input.shape[-1]
-            if len(input.shape)==2:
-                input_abs_max = torch.max(torch.abs(input[:,:dim//8]), dim=-1, keepdim=True).values*(1.125)
-            elif len(input.shape)==3:
-                input_abs_max = torch.max(torch.abs(input[:,:,:dim//8]), dim=-1, keepdim=True).values*(1.125)
-            else:
-                input_abs_max = torch.max(torch.abs(input[:,:,:,:dim//8]), dim=-1, keepdim=True).values*(1.125)
-            
+            # if len(input.shape)==2:
+            #     input_abs_max = torch.max(torch.abs(input[:,:dim//8]), dim=-1, keepdim=True).values*(1.125)
+            # elif len(input.shape)==3:
+            #     input_abs_max = torch.max(torch.abs(input[:,:,:dim//8]), dim=-1, keepdim=True).values*(1.125)
+            # else:
+            #     input_abs_max = torch.max(torch.abs(input[:,:,:,:dim//8]), dim=-1, keepdim=True).values*(1.125)
+            input_abs_max = torch.max(torch.abs(input), dim=-1, keepdim=True).values
             input_scale = (input_abs_max.clamp(min=1e-6) / 127.0)
             input = (input.float() / input_scale.float()).to(dtype)
             #Scale the weight to match the input scale, now default True, need to set dynamic_input=True for Linear
